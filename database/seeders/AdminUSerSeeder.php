083a90d423b6f6c77943c8961c6b2b701d029290
cheke <cheke_11@hotmail.com>
2026-05-18 19:26:39 -0600

    Se agrega Models de Region en el seeder de AdminUserSeeder

--- database/seeders/AdminUSerSeeder.php
+++ database/seeders/AdminUSerSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Region;
 
 class AdminUserSeeder extends Seeder
 {
